f9c562c8bea81168424ee23fdaac8434c4650406
crangoren <crangoren@gmail.com>
2022-01-18 18:06:29 +0300

    add

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -1,23 +1,3 @@
 public interface HashTable<K, V> {
 
-    boolean put(K key, V value);
-
-    V get(K key);
-
-    V remove(K key);
-
-    int size();
-
-    int count();
-
-    boolean isEmpty();
-
-    void display();
-
-    interface Entry<K, V> {
-        K getKey();
-        V getValue();
-
-        void setValue(V value);
-    }
 }
